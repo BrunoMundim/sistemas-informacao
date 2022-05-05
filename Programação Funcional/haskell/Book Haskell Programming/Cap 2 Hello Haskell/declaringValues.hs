b63cd949cb25d097f1fd139ed3d8f1760d449939
BrunoMundim <brunomundimfranco@gmail.com>
2022-05-05 16:02:23 -0300

    Começando Haskell

---
+++ Programação Funcional/haskell/Book Haskell Programming/Cap 2 Hello Haskell/declaringValues.hs
@@ -0,0 +1,10 @@
+module DeclaringValues where
+
+x :: Int
+x = 10 * 5 + y
+
+myResult :: Int
+myResult = x * 5
+
+y :: Int
+y = 10
---
+++ Programação Funcional/haskell/Book Haskell Programming/Cap 2 Hello Haskell/definingFunctions.hs
@@ -0,0 +1,5 @@
+triple :: Float -> Float
+triple x = x * 3
+
+circumference :: Floating a => a -> a
+circumference a = pi * (a * a)
